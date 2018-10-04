14674df445d0ef29c9e05b22df7092e98c167af8
bettyashley <bvaldivia8220@bths.edu>
2018-10-04 00:03:32 -0400

    Delete aboutme.js

--- public/aboutme.js
+++
@@ -1,6 +0,0 @@
-var btnElement = document.getElementById("tumblr-btn");
-btnElement.addEventListener("click", function visitPage(){
-  window.open('http://leihime.tumblr.com/');
-});
-
-//<button onclick="window.location='http://www.example.com';">Visit Page Now</button>
